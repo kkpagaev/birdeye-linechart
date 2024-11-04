6d8907561c5112adaefbbebc94fbec82fb487e0e
kkpagaev <kkpagaev@gmail.com>
2024-11-04 23:03:53 +0200

    created linechart with gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { useState, useEffect } from 'react';
+import {
+  LineChart,
+  Line,
+  Tooltip,
+} from 'recharts';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type HistoryPrice = {
+  unixTime: number
+  value: number
 }
 
-export default App
+function scaleData(prices: HistoryPrice[], toMin: number = 5, toMax: number = 19): HistoryPrice[] {
+  const values = prices.map((price) => price.value)
+  const min = Math.min(...values)
+  const max = Math.max(...values)
+  const delimeter = max - min
+  
+  return prices.map(({ value, unixTime }) => {
+    const newValue = toMin + (value - min) * (toMax - toMin) / delimeter
+    return {
+      value: newValue,
+      unixTime: unixTime,
+    }
+  })
+}
+
+function useData() {
+  const [data, setData] = useState<Array<HistoryPrice>>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("https://birdeye-proxy.jup.ag/defi/history_price?address=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&type=5m&time_to=1730740102")
+      .then((response) => response.json())
+      .then((json: {data: {items: HistoryPrice[]}}) => {
+        const data = json.data.items
+        setData(scaleData(data))
+        setLoading(false)
+      })
+  }, [])
+
+  return {
+    data,
+    loading,
+  }
+}
+
+const formatUnixTime = (unixTime: number) => {
+  const date = new Date(unixTime * 1000);
+  return `${date.getHours()}:${date.getMinutes()}`;
+};
+
+function App() {
+  const { data, loading } = useData();
+  if (loading) {
+    return <div>loading</div>
+  }
+  return (
+      <LineChart 
+        data={data}
+        height={24}
+        layout='horizontal' reverseStackOrder={false}
+        width={236}
+      >
+        <defs>
+          <linearGradient id="lineGradient" x1="0" y1="1" x2="1" y2="1">
+            <stop offset="0%" stopColor="#7EBFFA" />
+            <stop offset="100%" stopColor="#FA2155" />
+          </linearGradient>
+        </defs>
+        <Tooltip labelFormatter={formatUnixTime} />
+        <Line 
+          stroke="url(#lineGradient)"
+          yAxisId="2" type='linear' animationDuration={300}
+          dataKey="value"
+          from={0.99}
+          to={1}
+          className="gradient"
+          
+          strokeWidth={1}
+          legendType="line"
+          connectNulls={false}
+          height={14}
+          dot={false}
+
+        />
+      </LineChart>
+  );
+};
+
+export default App;
